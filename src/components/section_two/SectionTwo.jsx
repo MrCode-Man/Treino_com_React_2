214950d392fd38b7f3daf5bf8bab21e4e996b1c5
MrCode-Man <minimina722@gmail.com>
2026-01-13 12:15:05 -0300

    adicionado o Schedule que faltava na section two

--- src/components/section_two/SectionTwo.jsx
+++ src/components/section_two/SectionTwo.jsx
@@ -1,12 +1,14 @@
 import './SectionTwo.css'
 import MainText from "./MainText.jsx";
 import Service from "./service/Service.jsx";
+import Schedule from "./schedule/Schedule.jsx";
 
 function SectionTwo() {
     return (
         <div className="SectionTwo">
             <MainText/>
             <Service/>
+            <Schedule/>
         </div>
     )
 }
