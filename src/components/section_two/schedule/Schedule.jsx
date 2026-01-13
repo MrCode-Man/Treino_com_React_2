c715be47c319045a0c168ecd9c8bb253d954b33d
MrCode-Man <minimina722@gmail.com>
2026-01-13 12:16:02 -0300

    criada a section para Schedule que o site precisa

---
+++ src/components/section_two/schedule/Schedule.jsx
@@ -0,0 +1,13 @@
+import './Schedule.css'
+import ScheduleCards from "./scheduleCards/ScheduleCards.jsx";
+
+function Schedule() {
+    return (
+        <section className='schedule_section'>
+            <h2>Schedule</h2>
+            <ScheduleCards/>
+        </section>
+    )
+}
+
+export default Schedule;
