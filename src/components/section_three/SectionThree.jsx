d63bcf30896a1182ee55d242c7b7b0c3184ad892
MrCode-Man <minimina722@gmail.com>
2026-01-17 10:22:38 -0300

    adicionado o primeiro teste para o componente FAQ

--- src/components/section_three/SectionThree.jsx
+++ src/components/section_three/SectionThree.jsx
@@ -1,9 +1,12 @@
 import './SectionThree.css'
+import FAQ from './FAQ/FAQ.jsx'
+import {palavras}  from './FAQ/MatrizPalavras.js'
 
 function SectionThree() {
     return (
         <section className='SectionThree'>
-
+            <h2 className="titulo_section_three">FAQ</h2>
+            <FAQ titulo={palavras[0][0]} descricao={palavras[0][1]}/>
         </section>
     )
 }
